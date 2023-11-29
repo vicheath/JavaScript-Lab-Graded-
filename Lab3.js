6385595b0af58d2ec068eb479a255b9754b3d586
vicheath <vicheath.a.s@gmail.com>
2023-11-29 16:52:50 -0500

    first commit

---
+++ Lab3.js
@@ -0,0 +1,8 @@
+// R-ALAB 308.4.1: Working with Data Collections
+
+// Objectives
+// Use arrays to store ordered lists of data.
+// Use objects to store keyed lists of data.
+// Use conditional logic to process data.
+// Use loops to handle repetitive tasks.
+// Transform data according to specifications.
